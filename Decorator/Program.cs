7284c95198e65c562fd01562441333e276b200af
vkhoruzhenko <vadimuk@hotmail.co.uk>
2016-10-20 11:43:07 +0100

    Design Patterns p2

---
+++ Decorator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    { /// <summary>
+      /// Entry point into console application.
+      /// </summary>
+        static void Main()
+        {
+            // Create ConcreteComponent and two Decorators
+            var c = new ConcreteComponent();
+            var d1 = new ConcreteDecoratorA();
+            var d2 = new ConcreteDecoratorB();
+
+            // Link decorators
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+
+            d2.Operation();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
+    abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+    class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent.Operation()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Decorator' abstract class
+    /// </summary>
+    abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void Operation()
+        {
+            component?.Operation();
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteDecoratorA' class
+    /// </summary>
+    class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("ConcreteDecoratorA.Operation()");
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteDecoratorB' class
+    /// </summary>
+    class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("ConcreteDecoratorB.Operation()");
+        }
+
+        void AddedBehavior()
+        {
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            Console.ReadKey();
+        }
+    }
+
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    /// <summary>
+    /// The 'RealSubject' class
+    /// </summary>
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Called RealSubject.Request()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Proxy' class
+    /// </summary>
+    class Proxy : Subject
+    {
+        private RealSubject _realSubject;
+
+        public override void Request()
+        {
+            // Use 'lazy initialization'
+            if (_realSubject == null)
+            {
+                _realSubject = new RealSubject();
+            }
+
+            _realSubject.Request();
+        }
+    }
+}
